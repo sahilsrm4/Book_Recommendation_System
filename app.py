43f0af44365978e812045329914eea4d95354b2b
Sahil <sahilsharma9068730737@gmail.com>
2026-06-14 12:58:15 +0530

    changed port

--- app.py
+++ app.py
@@ -38,5 +38,7 @@ def recommend():
 
     print(data)
     return render_template('recommend.html', data = data)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
